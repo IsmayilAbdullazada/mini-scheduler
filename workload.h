ff289bcb1af1b9ef2b253a88f26f18e6df575a72
Yusif Askari <yusifaskeri2004@gmail.com>
2024-05-11 13:03:33 +0400

    Add workload header

--- workload.h
+++ workload.h
@@ -1,3 +1,12 @@
 #include <stdio.h>
 
 typedef struct workload_item_t workload_item;
+
+
+
+int get_workload_items(char* filename, workload_item* items, size_t max_items);
+
+workload_item* create_workload_item(int pid, int ppid, size_t ts, size_t tf, size_t idle, char* cmd, int prio);
+
+
+int compare_worlod_item(workload_item* item1, workload_item* item2);
